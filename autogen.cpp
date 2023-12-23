0d5457081e6a86b7a4c19b41229d461f4ddf3530
zlj-tyf <110816992+zlj-tyf@users.noreply.github.com>
2023-12-23 20:59:15 +0800

    fuck favicon

--- autogen.cpp
+++ autogen.cpp
@@ -1,10 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
-string name[]={"Xiaminglan","Zhangyao","Zhangshichao","Wangweiwei","Tanrihong","Wuyanming","Xujintao","Yaojiajia","Cuiyue","Liqianwen","Caiting","Chenxinhuan","Chenye","Gefang","Huangshan ","Lirongfang","Liulinqing","Pengwei","Shenxudong","Shiliangren","Wangqi","Wangyuan","Xufeng","Zhangjin","Zhanglu","Zhengyijie","Zhouguodong","Caozhipeng","Chenquan","Shankun","Fusheng","Guandayong","Guoyunji","Liuxinyue","Shenxuhui","Zhouzhengwei","Changwenyi","Liangting","Wangjing ","Zhangmengyun","Yaolu","Zhangguoxin","FunmanFung","YvooneChoo"};
+string name[]={"Xiaminglan","Zhangyao","Zhangshichao","Wangweiwei","Tanrihong","Wuyanming","Xujintao","Yaojiajia","Cuiyue","Liqianwen","Caiting","Chenxinhuan","Chenye","Gefang","Huangshan","Lirongfang","Liulinqing","Pengwei","Shenxudong","Shiliangren","Wangqi","Wangyuan","Xufeng","Zhangjin","Zhanglu","Zhengyijie","Zhouguodong","Caozhipeng","Chenquan","Shankun","Fusheng","Guandayong","Guoyunji","Liuxinyue","Shenxuhui","Zhouzhengwei","Changwenyi","Liangting","Wangjing","Zhangmengyun","Yaolu","Zhangguoxin","FunmanFung","YvooneChoo"};
 int main(){
 	int count=sizeof(name)/sizeof(name[0]);
 	string cmd="powershell -Command \"notepad++ D:/Users/HESY/Documents/GitHub/2024/";
-	string cmd2="index.html -n5 -c27\"";
+	string cmd2="index.html -n4 -c25\"";
 	for(int i=0;i<count;i++){
 		string end=cmd;
 		end+=name[i];
